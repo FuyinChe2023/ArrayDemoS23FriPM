bb5e2f76dfce8584cb09040c4b627dfeaf5cb11f
FuyinChe <chefuyin@outlook.com>
2023-05-19 13:03:32 -0400

    first commit

---
+++ src/arraysdemo/Student.java
@@ -0,0 +1,42 @@
+package arraysdemo;
+
+
+/**
+ *
+ * @author Alvin Che
+ */
+public class Student {
+    private String sId;
+    private String sName;
+
+    public Student(){
+        
+    }
+
+    public Student(String sId, String sName) {
+        this.sId = sId;
+        this.sName = sName;
+    }
+    
+    
+    
+    public String getsId() {
+        return sId;
+    }
+
+    public void setsId(String sId) {
+        this.sId = sId;
+    }
+
+    public String getsName() {
+        return sName;
+    }
+
+    public void setsName(String sName) {
+        this.sName = sName;
+    }
+    
+    
+    
+    
+}
